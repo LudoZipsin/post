cf86597c9c92d7575c00d3f8952eee076bf98000
Ludovic Zipsin <ludovic.j.r.zipsin@gmail.com>
2016-09-25 18:30:34 +0200

    progress on packages routes for alternates. TODO: linked queries with cat and tag

--- router/api/packages.js
+++ router/api/packages.js
@@ -1,5 +1,5 @@
 module.exports = function(router, modeler){
-	
+
     router.route('/')
     .get(function(req, res){
         modeler.Packages.forge()
@@ -135,62 +135,57 @@ module.exports = function(router, modeler){
         });
     });
 
-// NOT WORKING.... install.id is correct but the fetch on this don't work
+    var getInstall = function(packageId, installVesion){
+        return new modeler.Install()
+        .query({where: {package_id: packageId, version: installVesion}})
+        .fetch()
+        .then(function(model){
+            return model;
+        });
+    };
 
     router.route('/:id/installs/:version/alternates')
     .get(function(req, res){
-        modeler.Install.forge({package_id: req.params.id, version: req.params.version})
-        .fetch()
-        .then(function(install){
-            if (!install){
-                res.status(404).json({error: true, data: {}});
-            }
-            else {
-                console.log(install.id);
-                modeler.Alternates.forge({install_id: install.id})
-                .fetch()
-                .then(function(collection){
-                    console.log(collection.toJSON());
-                    if (! collection){
-                        res.status(404).json({error: true, data: {}});
-                    }
-                    else {
-                        res.json({error: false, data: collection.toJSON()});
-                    }
-                })
-                .catch(function(err){
-                    res.status(500).json({error: true, data: {message: err.message}});
-                });
-            }
+        getInstall(req.params.id, req.params.version)
+        .then(function(model){
+            var id = model.get('id');
+            modeler.Alternates.query({where: {install_id: id}})
+            .fetch()
+            .then(function(collection){
+                if (!collection){
+                    res.status(400).json({error: true, data: {}});
+                }
+                else {
+                    res.json({error: false, data: collection.toJSON()});
+                }
+            })
+            .catch(function(err){
+                res.status(500).json({error: true, data: {message: err.message}});
+            });
         })
         .catch(function(err){
             res.status(500).json({error: true, data: {message: err.message}});
         });
-    }); 
+    });
 
-    router.route('/:id/installs/:version/alternates/:id')
+    router.route('/:id/installs/:version/alternates/:alternateId')
     .get(function(req, res){
-        modeler.Install.forge({package_id: req.params.id, version: req.params.version})
-        .fetch()
-        .then(function(install){
-            if (!install){
-                res.status(404).json({error: true, data: {}});
-            }
-            else {
-                modeler.Alternate.forge({install_id: install.id})
-                .fetch()
-                .then(function(alternate){
-                    if (!alternate){
-                        res.status(404).json({error: true, data: {}});
-                    }
-                    else {
-                        res.json({error: false, data: alternate.toJSON()});
-                    }
-                })
-                .catch(function(err){
-                    res.status(500).json({error: true, data: {message: err.message}});
-                });
-            }
+        getInstall(req.params.id, req.params.version)
+        .then(function(model){
+            var installId = model.get('id');
+            modeler.Alternate.query({where: {id: req.params.alternateId, install_id: installId}})
+            .fetch()
+            .then(function(alternate){
+                if (!alternate){
+                    res.status(400).json({error: true, data: {}});
+                }
+                else {
+                    res.json({error: false, data: alternate.toJSON()});
+                }
+            })
+            .catch(function(err){
+                res.status(500).json({error: true, data: {message: err.message}});
+            });
         })
         .catch(function(err){
             res.status(500).json({error: true, data: {message: err.message}});
